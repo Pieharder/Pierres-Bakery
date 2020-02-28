c80e21edfc6043f1e2daaf1982dd37b037ec3810
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 12:12:27 -0800

    Add fields

--- models/bread.cs
+++ models/bread.cs
@@ -4,6 +4,11 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    
+    public int BreadPrice { get; set; }
+    public int BreadCount { get; set; }
+    public static void BreadPricing()
+    {
+
+    }
   }
 }
