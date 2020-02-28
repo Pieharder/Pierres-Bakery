afab1439dded21b2959301c4f49f385fdfb218c5
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 15:01:56 -0800

    Add price calculation

--- models/pastry.cs
+++ models/pastry.cs
@@ -5,17 +5,28 @@ namespace Bakery.Models
   public class Pastry
   {
     public int Price { get; set; }
-    public int Count { get; set; }
+    // public int Count { get; set; }
 
     public Pastry()
     {
       Price = 2;
     }
-    public static void PastryPricing()
+    public static void PastryInput()
     {
       Console.WriteLine("Please enter the number of pastries you would like:");
       string pastryCount = Console.ReadLine();
       int.Parse(pastryCount);
+    }
+    public static void PastryPrice(int pastryCount)
+    {
+      if (pastryCount == 3)
+      {
+        int pastryPrice = pastryCount - 1;
+      }
+      else
+      {
+        int pastryPrice = pastryCount * 2;
+      }
     }
   }
 }
