6c709aa6b74462e6fb3a3cac68047c2be56fb5f0
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 12:13:23 -0800

    Change code as appropriate.

--- program.cs
+++ program.cs
@@ -1,4 +1,5 @@
 using System;
+using Bakery.Models;
 
 namespace Bakery
 {
@@ -6,28 +7,59 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.Clear();
       Console.WriteLine("Welcome to Pierre's World Class Bakery!");
       Console.WriteLine("Today we have Fresh baked loaves of bread for a mere price of $5! We also have yummy pastries for $2!");
       Console.WriteLine("Today's specials are buy 2 get 1 free on bread as well as 3 pastries for $5!");
+      Console.WriteLine("Would you like to shop?");
+      Console.WriteLine("[SHOP] [EXIT]");
+      string shopStatus = Console.ReadLine();
+      
+      if (shopStatus.ToLower() == "shop")
+      {
+        Shop();
+      }
+      else if (shopStatus.ToLower() == "exit")
+      {
+        Console.WriteLine("Have a wonderful day and come see us again soon!");
+      }
+      else
+      {
+        Main();
+      }
+    }
+
+    public static void Shop()
+    {
       Console.WriteLine("Please Enter which you would like to buy:");
       Console.WriteLine("[BREAD] [PASTRY]");
       string productChoice = Console.ReadLine();
+      
       if (productChoice.ToLower() == "bread")
       {
-        Console.WriteLine("Please enter the number of loaves you would like:");
-        Console.ReadLine();
+        // Console.WriteLine("Please enter the number of loaves you would like:");
+        // string breadCount = Console.ReadLine();
         Console.WriteLine("Would you like to purchase anything else?");
         Console.WriteLine("[YES] [NO]");
         string backToMenu = Console.ReadLine();
       }
-      else if (productChoice.ToLower() == "Pastry")
+      else if (productChoice.ToLower() == "pastry")
       {
         Console.WriteLine("Please enter the number of pastries you would like:");
-        Console.ReadLine();
+        string pastryCount = Console.ReadLine();
         Console.WriteLine("Would you like to purchase anything else?");
         Console.WriteLine("[YES] [NO]");
         string backToMenu = Console.ReadLine();
       }
+      else
+      {
+        Shop();
+      }
+    }
+
+    public static void Checkout()
+    {
+
     }
   }
 }
