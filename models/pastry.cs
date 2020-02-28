b66a16b2be3b8d951dbfed2fafc308a895eb58e1
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 12:13:02 -0800

    Add fields, constructor and initial method.

--- models/pastry.cs
+++ models/pastry.cs
@@ -4,6 +4,18 @@ namespace Bakery.Models
 {
   public class Pastry
   {
-    
+    public int Price { get; set; }
+    public int Count { get; set; }
+
+    public Pastry()
+    {
+      Price = 5;
+    }
+    public static void PastryPricing()
+    {
+      Console.WriteLine("Please enter the number of loaves you would like:");
+      string breadCount = Console.ReadLine();
+      int.Parse(breadCount);
+    }
   }
 }
