b3331e8f215cf05ce8aeeb4f68d9b87bf30b8418
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 13:27:55 -0800

    Add method

--- models/bread.cs
+++ models/bread.cs
@@ -8,7 +8,9 @@ namespace Bakery.Models
     public int BreadCount { get; set; }
     public static void BreadPricing()
     {
-
+      Console.WriteLine("Please enter the number of loaves you would like:");
+      string breadCount = Console.ReadLine();
+      int.Parse(breadCount);
     }
   }
 }
