771a3d7d223117d295faebde55d258bac162ad81
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 15:55:13 -0800

    Further troubleshooting and refactoring

--- models/pastry.cs
+++ models/pastry.cs
@@ -5,27 +5,17 @@ namespace Bakery.Models
   public class Pastry
   {
     public int Price { get; set; }
-    // public int Count { get; set; }
+    public int Count { get; set; }
 
-    public Pastry()
-    {
-      Price = 2;
-    }
-    public static void PastryInput()
-    {
-      Console.WriteLine("Please enter the number of pastries you would like:");
-      string pastryCount = Console.ReadLine();
-      int.Parse(pastryCount);
-    }
     public static void PastryPrice(int pastryCount)
     {
       if (pastryCount == 3)
       {
-        int pastryPrice = pastryCount - 1;
+        int pastryFinal = (pastryCount * 2) - 1;
       }
       else
       {
-        int pastryPrice = pastryCount * 2;
+        int pastryFinal = pastryCount * 2;
       }
     }
   }
