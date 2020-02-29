38a5a95300df0228c0823b4f644f4336908bf1f6
Geoff Goetz <geoff.goetz503@gmail.com>
2020-02-28 17:45:11 -0800

    Finalized methods.

--- models/pastry.cs
+++ models/pastry.cs
@@ -1,15 +1,13 @@
-using System;
-
 namespace Bakery.Models
 {
   public class Pastry
   {
     public static int PPrice { get; set; }
     public static int PCount { get; set; }
-
     public static void PastryPricing(int Count)
     {
       PCount = Count;
+
       if (PCount >= 3)
       {
         PPrice = (PCount * 2) - 1;
